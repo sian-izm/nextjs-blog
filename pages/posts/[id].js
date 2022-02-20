e8206f3412ed282a31880995756e3eeb8b5acf3e
sian.izm <sian.izm@gmail.com>
2022-02-20 11:32:01 +0700

    appended head

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -18,14 +19,19 @@ export async function getStaticProps({ params }){
   }
 }
 
-export default function Post({ postData   }) {
-  return <Layout>
-    {postData.title}
-    <br />
-    {postData.id}
-    <br />
-    {postData.date}
-    <br />
-    <div dangerouslySetInnerHTML={{__html: postData.contentHtml }} />
-  </Layout>
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{__html: postData.contentHtml }} />
+    </Layout>
+  )
 }
