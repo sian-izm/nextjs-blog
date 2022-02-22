391e6439f1ed9f0d68344363b729b6758a96c495
sian.izm <sian.izm@gmail.com>
2022-02-22 23:02:41 +0700

    installed apollo client

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,30 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import { getSortedPostsData } from '../lib/posts'
 import Date from '../components/date'
+import client from '../apollo-client'
+import { gql } from '@apollo/client'
 
 export async function getStaticProps () {
   const allPostsData = getSortedPostsData()
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+        }
+      }
+    `,
+  })
   return {
     props: {
-      allPostsData
+      allPostsData,
+      countries: data.countries.slice(0, 4),
     }
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData, countries }) {
   return (
     <Layout home>
       <Head>
@@ -47,6 +60,16 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
+      </section>
+      <section className={`${utilStyles.heading} ${utilStyles.padding1px}`}>
+        {countries.map((country) => (
+          <div key={country.code}>
+            <h3><a href="#country-name" aria-hidden="true" class="aal_anchor" id="country-name"><svg aria-hidden="true" class="aal_svg" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a></h3>
+            <p>
+              {country.code} = {country.emoji}
+            </p>
+          </div>
+        ))}
       </section>
     </Layout>
   )
