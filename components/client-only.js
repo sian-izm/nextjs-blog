62908c2098a63da4ddc4026494dd11a3bd449650
sian.izm <sian.izm@gmail.com>
2022-02-23 21:05:16 +0700

    define client side component

---
+++ components/client-only.js
@@ -0,0 +1,13 @@
+export default function ClientOnly({ children, ...delegated }) {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div {...delegated}>{children}</div>
+}
