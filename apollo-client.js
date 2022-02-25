53f1aa52d8610d28d3bef91e50db104da389eba6
sian.izm <sian.izm@gmail.com>
2022-02-25 19:50:31 +0700

    arrange port to 3001

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: "https://countries.trevorblades.com",
+  uri: "http://localhost:3000/graphql",
+  // uri: "https://countries.trevorblades.com",
   cache: new InMemoryCache(),
 });
 
