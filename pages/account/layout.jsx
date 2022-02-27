2746bdc7e8af4e2795bd0f58e5e2615126b8e026
sian.izm <sian.izm@gmail.com>
2022-02-27 22:01:18 +0700

    define next.config.js

---
+++ pages/account/layout.jsx
@@ -0,0 +1,17 @@
+import { useRouter } from "next/router";
+
+function Layout( {children }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (userService.userValue) {
+      router.push('/');
+    }
+  }, []);
+
+  return (
+    <div className="col-md-6 offset-md-3 mt-5">
+      {children}
+    </div>
+  );
+}
