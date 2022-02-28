e1480e3cc10ee5dffc3687b77f6c9f9caa83c475
sian.izm <sian.izm@gmail.com>
2022-02-28 21:58:23 +0700

    added login on user service

--- services/user.service.js
+++ services/user.service.js
@@ -23,6 +23,34 @@ function login(username, password) {
       userSubject.next(user);
       localStorage.setItem('user', JSON.stringify(user));
 
-      return user      ;
+      return user;
     });
+}
+
+function register(user) {
+  return fetchWrapper.post(`${baseUrl}/register`, user);
+}
+
+function getAll() {
+  return fetchWrapper.get(baseUrl);
+}
+
+function getById() {
+  return fetchWrapper.get(`${baseUrl}/${id}`);
+}
+
+function update(id, params) {
+  return fetchWrapper.put(`${baseUrl}${id}`, params)
+    .then(x => {
+      // update stored user if the logged in user updated their own record
+      if (id === userSubject.value.id) {
+        // const local storage
+        const user = { ...userSubject.value, ...params };
+        localStorage.setItem('user', JSON.stringify(user));
+
+        // publish updated user to subscribers
+        userSubject.next(user);
+      }
+      return x;
+    })
 }
