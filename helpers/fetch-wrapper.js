eba504c64401fb8a17f63cb0d2703eae6716be1e
sian.izm <sian.izm@gmail.com>
2022-03-02 22:42:48 +0700

    define fetch-wrapper

---
+++ helpers/fetch-wrapper.js
@@ -0,0 +1,65 @@
+import getConfig from "next/config";
+import { userService } from '../services/user.service';
+const { publicRuntimeConfig } = getConfig();
+
+export const fetchWrapper = {
+  get,
+  post,
+//  put,
+  delete: _delete
+};
+
+function get(url) {
+  const requestOptions = {
+    method: 'GET',
+    headers: authHeader(url)
+  };
+  return fetch(url, requestOptions).then(handleResponse);
+}
+
+function post(url, body) {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json', ...authHeader(url) },
+    credentials: 'include',
+    body: JSON.stringify(body)
+  };
+  return fetch(url, requestOptions).then(handleResponse);
+}
+
+function _delete(url) {
+  const requestOptions = {
+    method: 'DELETE',
+    headers: authHeader(url)
+  };
+  return fetch(url, requestOptions).then(handleResponse);
+}
+
+function authHeader(url) {
+  const user =userService.userValue;
+  const isLoggedIn = user && user.token;
+  const isApiUrl = url.startsWith(publicRuntimeConfig.apiUrl);
+
+  if (isLoggedIn && isApiUrl) {
+    return { Authorization: `Bearer ${user.token}` };
+  } else {
+    return {};
+  }
+}
+
+function handleResponse(response) {
+  return response.text().then(text => {
+    const data = text && JSON.parse(text);
+
+    if(!response.ok) {
+      if ([401, 403].includes(response.status) && userService.userValue) {
+        userService.logout();
+      }
+
+      const error = (data && data.message) || response.statusText;
+      return Promise.reject(error);
+    }
+
+    return data;
+  })
+}
