4854f4a25ab40422e7122388ab18bc09941d10dc
sian.izm <sian.izm@gmail.com>
2022-03-19 23:43:32 +0900

    remove registration from login function

--- pages/account/login.jsx
+++ pages/account/login.jsx
@@ -4,20 +4,9 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import { Layout } from '../../components/account/layout';
 import { Link } from '../../components/link';
-import { gql, useMutation } from '@apollo/client';
 import { login } from '../../lib/users';
 
-const QUERY = gql`
-  mutation signup($name: String!, $password: String!) {
-    signup(input: {name: $name, password: $password}) {
-      name
-      id
-    }
-  }
-  `;
-
 export default function Login() {
-  console.log('Login called');
   const router = useRouter();
 
   // from validation rules
@@ -30,19 +19,9 @@ export default function Login() {
   // get functions to build form with useForm() hook
   const { register, handleSubmit, formState } = useForm(formOptions);
   const { errors } = formState;
-  const [ signup, { data, loading, error }] = useMutation(QUERY);
-
-  if (loading) {
-    return <h2><a href="#loading" aria-hidden="true" className="aal_anchor" id="loading"><svg aria-hidden="true" className="aal_svg" height="16" version="1.1" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg></a>Loading...</h2>;
-  }
-  if (error) {
-    console.error(error);
-    return null;
-  }
 
   const onSubmit = async ({username, password}) => {
     event.preventDefault();
-    await signup({ variables: { name: username, password: password }});
     await login(username, password);
     await router.reload("/");
   }
