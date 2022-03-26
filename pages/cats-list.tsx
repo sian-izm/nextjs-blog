c089b09af5d1a04db054df40bbd37d09e4dc5f8e
sian.izm <sian.izm@gmail.com>
2022-03-26 23:59:53 +0900

    add indent

--- pages/cats-list.tsx
+++ pages/cats-list.tsx
@@ -20,6 +20,7 @@ export default function CatsList() {
   );
 }
 
+
 export const getServerSideProps = withIronSessionSsr(
   async function getServerSideProps({ req }) {
     const user = req.session.user;
