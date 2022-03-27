662b7c7850387d611b779bec127d949af959dd5e
sian.izm <sian.izm@gmail.com>
2022-03-28 00:14:50 +0900

    get user data from DB for api/login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,19 +1,26 @@
+import { Octokit } from "octokit";
 import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { sessionOptions } from "../../lib/session";
+const oktkit = new Octkit();
 
-export default withIronSessionApiRoute(
-  async function loginRoute(req, res) {
+export default withIronSessionApiRoute(loginRoute, sessionOptions);
+
+async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
+  const { username } = await req.body;
+  try {
+    const {
+      data: { name, password, id }
+    } = await octokit.res.users.getByName({ username });
+
+    const user = { password: "PASSWORD", id: 100 } as User;
     req.session.user = {
       id: 230,
       admin: true,
     };
     await req.session.save();
-    res.send({ ok: true });
-  },
-  {
-    cookieName: "hogecookie",
-    password: "update me password",
-    cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
-    }
-  },
-);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+}
