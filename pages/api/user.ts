4451b6e48d9a24283ab2eb273dae72155f666614
sian.izm <sian.izm@gmail.com>
2022-03-29 00:04:53 +0900

    defined user api

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,14 +1,25 @@
 import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { sessionOptions } from "../../lib/session";
 
-export default withIronSessionApiRoute(
-  function userRoute(req, res) {
-    res.send({ user: req.session.user });
-  },
-  {
-    cookieName: "hogecookie",
-    password: "update me password",
-    cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
-    }
-  },
-);
+export type User = {
+  isLoggedIn: boolean;
+  login: string;
+  avatarUrl: string;
+}
+
+export default withIronSessionApiRoute(userRoute, sessionOptions);
+async function userRoute(req: NextApiRequest, res: NextApiResponse<User>) {
+  if (req.session.user) {
+    res.json({
+      ...req.session.user,
+      isLoggedIn: true,
+    });
+  } else {
+    res.json({
+      isLoggedIn: false,
+      login: "",
+      avatarUrl: "",
+    });
+  }
+}
