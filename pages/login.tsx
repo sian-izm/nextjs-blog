63ba809f06afe501bb1ba9e1acf2f68c576823e9
sian.izm <sian.izm@gmail.com>
2022-03-30 19:39:28 +0900

    define login page

---
+++ pages/login.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import Layout from "../components/layout";
+import Form from "../components/form";
+import fetchJson, { FetchError } from "lib/fetchJson";
+
+export default function Login() {
+  const { mutateUser } = useUser({
+    redirectTo: "profile-sg",
+    redirectIfFound: true,
+  });
+
+  const [errorMsg, setErrorMsg] = useState("");
+
+  return (
+    <Layout>
+      <div className="login">
+        <Form errorMessage={errorMsg} onSubmit={async function handleSubmit(event){
+          event.preventDefault();
+
+          const body = {
+            username: event.currentTarget.username.value,
+          };
+
+          try {
+            mutateUser(
+              await fetchJson("/api/login", {
+                method: "POST",
+                headers: { "Content=type": "application/json" },
+                body: JSON.stringify(body),
+              }),
+            );
+          } catch (error) {
+            if (error instanceof FetchError) {
+              setErrorMsg(error.data.message);
+            } else {
+              console.error("An unexpected error happened:", error);
+            }
+          }
+        }}
+        />
+      </div>
+      <style jsx>{`
+        .login {
+          max-width: 21rem;
+          margin: 0 auto;
+          padding: 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+      `}</style>
+    </Layout>
+  );
+}
